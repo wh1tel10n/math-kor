c729e5317765cb8fc51d27fef30c49643d8b828e
James Lim <email@wanseob.com>
2018-11-13 23:36:19 +0900

    Setup package structure

---
+++ math_kor/advanced.py
@@ -0,0 +1,14 @@
+"""
+복잡한 수학 공식을 한글로 풀 수 있습니다.
+"""
+def 최대공약수(a, b):
+    """
+    최대공약수를 반환하는 함수입니다. 
+    >>> 최대공약수(18,30)
+    6
+    >>> 최대공약수(1.3, 19)
+    Traceback (most recent call last):
+        ...
+    ValueError: params should be an integer
+    """
+    return 0

3e88d516de487e22b5f9b67e8cf710231ecbeddc
wh1tel10n <48845968+wh1tel10n@users.noreply.github.com>
2019-03-29 09:10:26 +0900

    곱하기, 나누기 추가
    수업시간에 많이 도와주셔서 감사드립니다.^^

---
+++ basic.py
@@ -0,0 +1,30 @@
+"""
+한글로 수학연산식을 작성하기 위해 만들어진 패키지입니다.
+"""
+def 더하기(a,b):
+    """Returns the sum of a and b
+    >>> 더하기(1,2)
+    3
+    """
+    return a + b
+
+def 빼기(a,b):
+    """Returns the sum of a and b
+    >>> 빼기(3,2)
+    1
+    """
+    return a - b
+
+def 곱하기(a,b):
+    """Returns the multiply of a and b
+    >>> 곱하기(3,2)
+    6
+    """
+    return a * b
+
+def 나누기(a,b):
+    """Returns the divide of a and b
+    >>> 나누기(10,5)
+    5
+    """
+    return a / b
